0410b33866679fbeb606c4d706566046766a25d8
Ostap <Ostapkriven13@gmail.com>
2023-09-23 23:23:07 +0300

    add code

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const Searchbar = ({ onSubmit }) => {
+  const [query, setQuery] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(query);
+    setQuery('');
+  };
+
+  return (
+    <header className="searchbar">
+      <form className="form" onSubmit={handleSubmit}>
+        <button type="submit" className="button">
+          <span className="button-label">Search</span>
+        </button>
+        <input
+          className="input"
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
+
+export default Searchbar;
